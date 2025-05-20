882dabcb5a916d6616054e49d24441810d113a1f
NikVince <165183152+NikVince@users.noreply.github.com>
2025-05-20 18:01:26 +0200

    UPDATED problems.js to implement logic that passes test for funcion 'doubler(nums)'.

--- problems.js
+++ problems.js
@@ -27,6 +27,11 @@ function doubler(nums) {
     /* Takes an array of numbers and returns a new array where every element of
     the original array is multiplied by 2. */
     // Your code here
+    let newArray = [];
+    for (let i = 0; i < nums.length; i++) {
+        newArray.push(nums[i] * 2);
+    }
+    return newArray;
 };
 
 function combineArrays(arr1, arr2) {
